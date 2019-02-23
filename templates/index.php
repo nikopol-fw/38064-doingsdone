08617e8190a420c4bb0ac4fdff1e466777ac39be
Nikolay Dumchev <nikolay.dumchev@gmail.com>
2019-02-23 18:00:24 +0300

    Добавляет проверку дат окончания задач

--- templates/index.php
+++ templates/index.php
@@ -1,46 +1,53 @@
 <h2 class="content__main-heading">Список задач</h2>
 
 <form class="search-form" action="index.php" method="post">
-    <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
+  <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
 
-    <input class="search-form__submit" type="submit" name="" value="Искать">
+  <input class="search-form__submit" type="submit" name="" value="Искать">
 </form>
 
 <div class="tasks-controls">
-    <nav class="tasks-switch">
-        <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
-        <a href="/" class="tasks-switch__item">Повестка дня</a>
-        <a href="/" class="tasks-switch__item">Завтра</a>
-        <a href="/" class="tasks-switch__item">Просроченные</a>
-    </nav>
+  <nav class="tasks-switch">
+    <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
+    <a href="/" class="tasks-switch__item">Повестка дня</a>
+    <a href="/" class="tasks-switch__item">Завтра</a>
+    <a href="/" class="tasks-switch__item">Просроченные</a>
+  </nav>
 
-    <label class="checkbox">
-        <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-        <input class="checkbox__input visually-hidden show_completed" type="checkbox"<?php
-            if ($show_complete_tasks === 1):?><?= ' checked'; ?><?php endif; ?>>
-        <span class="checkbox__text">Показывать выполненные</span>
-    </label>
+  <label class="checkbox">
+    <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
+    <input class="checkbox__input visually-hidden show_completed" type="checkbox"<?php
+    if ($show_complete_tasks === 1):?><?= ' checked'; ?><?php endif; ?>>
+    <span class="checkbox__text">Показывать выполненные</span>
+  </label>
 </div>
 
 <table class="tasks">
-<?php foreach ($tasks as $key => $value): ?>
+  <?php foreach ($tasks as $key => $value): ?>
     <?php if ($show_complete_tasks === 1 || ($show_complete_tasks === 0 && $value['is_done'] === false)): ?>
-    <tr class="tasks__item task<?php
-        if ($value['is_done'] === true): ?><?= ' task--completed'; ?><? endif; ?>">
+      <tr class="tasks__item task<?php
+      if ($value['is_done'] === true):
+        echo " task--completed";
+      endif;
+
+      if (checkTask24h($value['done_date'])):
+        echo " task--important";
+      endif;
+      ?>">
         <td class="task__select">
-            <label class="checkbox task__checkbox">
-                <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1"<?php
-                    if ($value['is_done'] === true): ?><?= ' checked'; ?><?php endif; ?>>
-                <span class="checkbox__text"><?= htmlspecialchars($value['title']); ?></span>
-            </label>
+          <label class="checkbox task__checkbox">
+            <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1"<?php
+            if ($value['is_done'] === true): ?><?= ' checked'; ?><?php endif; ?>>
+            <span class="checkbox__text"><?= htmlspecialchars($value['title']); ?></span>
+          </label>
         </td>
 
         <td class="task__file">
-            <a class="download-link" href="#">Home.psd</a>
+          <a class="download-link" href="#">Home.psd</a>
         </td>
 
         <td class="task__date"><?= htmlspecialchars($value['done_date']); ?></td>
-    </tr>
+      </tr>
     <?php endif; ?>
-<?php endforeach; ?>
+  <?php endforeach; ?>
 </table>
--- utils/functions.php
+++ utils/functions.php
@@ -1,37 +1,63 @@
 <?php
 /**
  * Функция выводит шаблон
+ *
  * @param string $name Название/путь шаблона от папки /templates/
- * @param array $data Ассоциативный массив с данными для шаблона
+ * @param array  $data Ассоциативный массив с данными для шаблона
+ *
  * @return string итоговый HTML-код с подставленными данными
  */
 function render_template ($name, $data = null) {
-    $path = 'templates/' . $name;
-    $result = '';
+  $path   = __DIR__ . '/../templates/' . $name;
+  $result = '';
 
-    if (is_readable($path)) {
-        ob_start();
-        extract($data);
+  if (is_readable($path)) {
+    ob_start();
+    extract($data);
 
-        require $path;
-        $result = ob_get_clean();
-    }
+    require $path;
+    $result = ob_get_clean();
+  }
 
-    return $result;
+  return $result;
 }
 
 /**
  * Получить количество категорий
+ *
  * @param $tasks
  * @param $category
+ *
  * @return int
  */
 function get_tasks_count ($tasks, $category) {
-    $count = 0;
-    foreach ($tasks as $value) {
-        if ($value['category'] === $category) {
-            $count++;
-        }
+  $count = 0;
+  foreach ($tasks as $value) {
+    if ($value['category'] === $category) {
+      $count++;
     }
-    return $count;
+  }
+  return $count;
+}
+
+/**
+ * Проверяет время до окончания задачи
+ * Больше 24 часов = false
+ * Меньше или равно = true
+ *
+ * @param string $task_date Дата окончания задачи
+ *
+ * @return bool
+ */
+function checkTask24h ($task_date) {
+  $DAY_TIME = 86400;
+  $isEnding = false;
+
+  $time_stamp = strtotime($task_date) ?: 0;
+  $time_diff  = $time_stamp - time();
+  if ($time_diff > 0 && $time_diff <= $DAY_TIME) {
+    $isEnding = true;
+  }
+
+  return $isEnding;
 }
